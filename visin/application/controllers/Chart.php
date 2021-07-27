aa6985469e52db365eade28914f9873df2c6f9a1
umi <umi1800016082@webmail.uad.ac.id>
2021-07-27 16:20:21 +0700

    UAS_VISUALISASI_INFORMASI

---
+++ visin/application/controllers/Chart.php
@@ -0,0 +1,48 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Chart extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$title='Grafik Data Persentase';
+		$data['grafik data']="[
+			['Mushrooms', 3],
+			['Onions', 1],
+			['Olives', 1],
+			['Zucchini', 1],
+			['Pepperoni', 2]
+		]";
+		$data['grafik_data']=json_encode($grafik_data); 
+		$data['title']=$title;
+		$this->load->view('chart',$data);
+	}  
+	
+	function grafik()
+	{
+		$chartData=[
+			['Mushrooms', 3],
+			['Onions', 1],
+			['Olives', 1],
+			['Zucchini', 1],
+			['Pepperoni', 2]
+		];
+		$data['PieChartData']=json_encode  ($chartData);
+		$this->load->view('grafik',$data);
+	}  
+}
